9f89b2058b1f2659228cb7fb3ea437aac3d0e8a6
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-05-12 12:21:08 -0700

    customized tooltip

--- src/components/Dashboard/Likes/Chart/CustomToolTip.jsx
+++ src/components/Dashboard/Likes/Chart/CustomToolTip.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import './CustomToolTip.css';
 
 class CustomToolTip extends Component {
   render() {
-    console.log(this.props);
-    return (
-      <div>
-        Hello
-      </div>
-    );
+    const imgArr = this.props.payload[0];
+    if (imgArr) {
+      const img = imgArr.payload;
+      return (
+        <div className="floating-thumbnail p-2 text-center">
+          <img src={img.images.thumbnail.url} alt="Featured image" width="50" className="rounded"/>
+          <div>
+            <span><i className="fa fa-heart text-danger"></i> {img.likes.count}</span>
+          </div>
+        </div>
+      );
+    } else {
+        return null;
+    }
   }
 }
 
